2d4819263100057abde4f865d214bf3da22d0006
mdzapeer <mmudassers@gmail.com>
2018-05-29 22:56:32 +0400

    styling

--- App8FinancialCharts.py
+++ App8FinancialCharts.py
@@ -10,17 +10,12 @@ data, meta_data = cc.get_digital_currency_daily(symbol='BTC', market='USD')
 data["Date"]=pandas.to_datetime(data.index)
 data1=data[-50:]
 
-p=figure(x_axis_type='datetime', width=1000, height=300)
+p=figure(x_axis_type='datetime', width=1000, height=300,responsive=True)
 p.title.text="Candlestick Chart"
 
 w = 12*60*60*1000 # half day in ms
 
-
-
-
 #df=pandas.read_json("https://www.alphavantage.co/query?function=DIGITAL_CURRENCY_WEEKLY&symbol=BTC&market=USD&apikey=K53QQNRE6X9S2YCI", )
-
-
 
 data1["Date"]=pandas.to_datetime(data1.index)
 
@@ -32,5 +27,5 @@ p.segment(data1.Date,data1["2b. high (USD)"],data1.Date,data1["3b. low (USD)"],c
 p.vbar(data1.Date[increase],w,data1["1a. open (USD)"][increase],data1['4b. close (USD)'][increase],fill_color="green", line_color="black")
 p.vbar(data1.Date[decrease],w,data1["1a. open (USD)"][decrease],data1['4b. close (USD)'][decrease],fill_color="red", line_color="black")
 
-output_file("test.html", title="sampletest")
+output_file("test1.html", title="sampletest")
 show(p)
